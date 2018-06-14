61fbf6d3c0f2c9067036d9d799c66078fc18b6c6
shane <sexton.shane13@gmail.com>
2018-06-14 15:22:10 -0500

    final product

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -5,12 +5,20 @@ class PayAccountsController < ApplicationController
   end
 
   def show
+  	@account= PayAccount.find_by(id: params[:id])
   end
 
   def create
+  	@account_number= rand 111111111111...999999999999
+  	@new_account = PayAccount.create(nickname: params[:nickname], account_number: @account_number, account_type: params[:account_type].to_i)
+  	@new_account.save
+  	return redirect_to root_path
   end
 
   def destroy
+  	@account = PayAccount.find(params[:id])
+  	@account.destroy
+    return redirect_to root_path
   end
 
 end
